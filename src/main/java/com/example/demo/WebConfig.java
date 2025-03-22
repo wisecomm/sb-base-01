d44845578c309eee030d9770773b512636ef2b67
danyoh <danyoh#gmail.com>
2025-03-22 21:30:59 +0900

    1

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
